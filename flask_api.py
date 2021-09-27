8bae375b82e453544384e928daebb55b842a85ad
nishitjain1204 <yonishitjain123456@gmail.com>
2021-09-28 01:16:32 +0530

    removed debug mode

--- flask_api.py
+++ flask_api.py
@@ -1,6 +1,7 @@
 from flask import Flask , jsonify
 from flask_restx import Resource, Api
 from newsscrapper import scraper
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,4 +17,5 @@ class home(Resource):
         return {'hello':'world'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=PORT, debug=False)
